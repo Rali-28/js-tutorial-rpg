d7769f7d8a44994b98a0950f9fd965d3c99d6a59
Rali-28 <113418936+Rali-28@users.noreply.github.com>
2023-01-19 20:21:23 +0800

    Stats Variables
    added the variables for game stats

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Stats Variables */
+
+// player's xp
+let xp = 0;
+//player's health
+let health = 100;
+// player's gold
+let gold = 50;
+// player's current weapon/s
+let currentWeapon = 0;
+// variable when fighting
+let fighting;
+// determines the monster's helath
+let monsterHealth;
+// Arrays for player's inventory
+let inventory = ["stick"];
+
+/* End Stats Variables */
+
